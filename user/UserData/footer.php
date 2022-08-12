6a632f3bc8975014d52792ae2b5de22a60f2fa18
Hrithik Singh <hrithik@acrobuild.ai>
2022-08-12 18:48:05 +0530

    pushing code into repository

---
+++ user/UserData/footer.php
@@ -0,0 +1,26 @@
+</div>
+
+
+
+<footer class="footer">
+
+    <div class="container-fluid">
+        <div class="row text-muted">
+            <div class="col-6 text-left">
+                <p class="mb-0">
+                    <a href="../../index.php" class="text-muted"><strong><?php echo BANKNAME ?>
+                        </strong></a> &copy
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</div>
+</div>
+<!-- /#page-content-wrapper -->
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- javascript -->
